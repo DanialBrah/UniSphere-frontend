8801ae8a4cb65a4bbd868b24c974281b12d01696
DanialBrah <danialx003@gmail.com>
2026-06-01 23:38:16 +0800

    fix: remove fallback

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,5 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router'
 
 export default function App() {
-  return <RouterProvider router={router} fallbackElement={<div className="min-h-screen bg-white" />} />
+  return <RouterProvider router={router} />
 }
